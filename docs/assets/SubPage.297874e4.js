e657ead86302905c68835e41404ea18a8011888c
jude <jude.sh@daum.net>
2022-12-12 11:07:22 +0900

    autoprefixer code move

---
+++ docs/assets/SubPage.297874e4.js
@@ -0,0 +1,3 @@
+import{S as n,i as r,s as o,e as c,a as d,n as s,b as v,d as _,c as g,f as u}from"./index.0237c1d3.js";function f(i){let e,t;return{c(){e=c("div"),e.innerHTML=`<h2 class="svelte-12oraxg">SubPage</h2> 
+  <div class="img_svelte svelte-12oraxg"></div> 
+  <div class="img_vite svelte-12oraxg"></div>`},m(a,l){d(a,e,l)},p:s,i(a){t||v(()=>{t=g(e,u,{}),t.start()})},o:s,d(a){a&&_(e)}}}class p extends n{constructor(e){super(),r(this,e,null,f,o,{})}}export{p as default};
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    devSourcemap: true,
-  },
   plugins: [
     svelte(),
     legacy({
       target: ["defaults", "not IE 11"],
     }),
   ],
+  css: {
+    devSourcemap: true,
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   base: "./",
   build: {
     outDir: "docs",
